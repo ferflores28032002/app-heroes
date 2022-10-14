5026e317b0b6d27317fd591a3973a180801f601f
fernando flores <martinjanns2000@gmail.com>
2022-10-14 04:59:32 -0600

    nuevos cambios

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./Sass/index.scss";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { BrowserRouter } from "react-router-dom";
+//Main 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
